9f99efe8112fa29a437f0d381ca6bdcd1d472f61
Nicolas18RO <nicorodrigueopayome@gmail.com>
2025-09-17 23:30:17 -0500

    First Commit

--- codigo.js
+++ codigo.js
@@ -1,38 +1,33 @@
 function mifuncion(){
-      let vc=0;
-      let mensaje = document.getElementById("mse").value;
-      respuesta = "";
-      let salida = ""
-      let caracter = "";
-      let numero = 0;
-      for ( vc=0; vc < mensaje.length ; vc++ ){
+    let mensaje = document.getElementById("mse").value;
+    let respuesta = "";
+    let salida = ""
+    let caracter = "";
+    let numero = 0;
+    for ( let vc = 0; vc < mensaje.length ; vc++ ){
 
-            respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
-            numero =  mensaje[vc].charCodeAt(0) + 10;
-            caracter = String.fromCharCode (numero);
-            salida = salida + caracter;
-            
-            
+        respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
+        numero =  mensaje[vc].charCodeAt(0) + 10;
+        salida = String.fromCharCode (numero);
+        salida = salida + caracter;
+}
 
-      }
-
-      document.getElementById("se").innerHTML = salida;
+    document.getElementById("se").innerHTML = salida;
 }
 
 
 function mifuncion2(){
-      let vc=0;
-      let mensaje = document.getElementById("me").value;
-      respuesta = "";
-      let salida = ""
-      let caracter = "";
-      let numero = 0;
-      for ( vc=0; vc < mensaje.length ; vc++ ){
-            //respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
-            numero =  mensaje[vc].charCodeAt(0) - 10;
-            caracter = String.fromCharCode (numero);
-            salida = salida + caracter;
-      }
+    let mensaje = document.getElementById("me").value;
+    let respuesta = "";
+    let salida = ""
+    let caracter = "";
+    let numero = 0;
+    for ( let vc = 0; vc < mensaje.length ; vc++ ){
+        //respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
+        numero =  mensaje[vc].charCodeAt(0) - 10;
+        caracter = String.fromCharCode (numero);
+        salida = salida + caracter;
+    }
 
-      document.getElementById("sse").innerHTML = salida;
+    document.getElementById("sse").innerHTML = salida;
 }
