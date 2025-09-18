e94436760a5eb544481bac43ed09980e0d1b68d8
Nicolas18RO <nicorodrigueopayome@gmail.com>
2025-09-18 00:06:46 -0500

    First Commit

--- codigo.js
+++ codigo.js
@@ -1,14 +1,15 @@
 function mifuncion(){
+    let vc=0;
     let mensaje = document.getElementById("mse").value;
     let respuesta = "";
     let salida = ""
     let caracter = "";
     let numero = 0;
-    for ( let vc = 0; vc < mensaje.length ; vc++ ){
+    for ( vc=0; vc < mensaje.length ; vc++ ){
 
         respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
         numero =  mensaje[vc].charCodeAt(0) + 10;
-        salida = String.fromCharCode (numero);
+        caracter = String.fromCharCode (numero);
         salida = salida + caracter;
 }
 
@@ -17,17 +18,18 @@ function mifuncion(){
 
 
 function mifuncion2(){
+    let vc=0;
     let mensaje = document.getElementById("me").value;
-    let respuesta = "";
+    respuesta = "";
     let salida = ""
     let caracter = "";
     let numero = 0;
-    for ( let vc = 0; vc < mensaje.length ; vc++ ){
+    for ( vc=0; vc < mensaje.length ; vc++ ){
         //respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
         numero =  mensaje[vc].charCodeAt(0) - 10;
         caracter = String.fromCharCode (numero);
         salida = salida + caracter;
-    }
+}
 
     document.getElementById("sse").innerHTML = salida;
 }
